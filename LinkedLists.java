d6eb318af3a8e0cf74d7dde00ed58225db981c6a
Netra-sh <67039687+Netra-sh@users.noreply.github.com>
2021-06-16 01:29:22 +0530

    Update LinkedLists.java

--- LinkedLists.java
+++ LinkedLists.java
@@ -1,45 +1,26 @@
-package com.example;
-
 import java.util.*;
 public class LinkedList {
-	
-	public static void main(String args[]) {
-		
-		 LinkedLists<String> ll=new LinkedLists<String>();  
-		 
-		 
-		 ll.add("Netra");
-		 ll.add("xyz");
-		 ll.add("yella");
-		 
-		 ll.offer("ss");
-		 ll.offer("nn"); 
-
-		 
-		 ll.addFirst("Netra123");  //implements deque inteface
-		 ll.addLast("cs");
-		 
-		 ll.peekLast();  //it return last element
-		 
-		 ll.pollFirst();
-		 
-		 ll.removeLast();
-		 for(String e:ll) {
-			 System.out.println(e);
-		 }
-		 
-	
-		
-		 
-		 //descending
-		 
-		 System.out.println("------------------");
-		 
-		 Iterator itr = ll.descendingIterator();
-		 
-		 while(itr.hasNext()) {
-			 System.out.println(itr.next());
-		 }
-	}
-
-}
+	public static void main(String args[])
+    {
+        // Creating object of the
+        // class linked list
+        LinkedList<String> ll
+            = new LinkedList<String>();
+  
+        // Adding elements to the linked list
+        ll.add("A");
+        ll.add("B");
+        ll.addLast("C");
+        ll.addFirst("D");
+        ll.add(2, "E");
+  
+        System.out.println(ll);
+  
+        ll.remove("B");
+        ll.remove(3);
+        ll.removeFirst();
+        ll.removeLast();
+  
+        System.out.println(ll);
+    }
+}	
